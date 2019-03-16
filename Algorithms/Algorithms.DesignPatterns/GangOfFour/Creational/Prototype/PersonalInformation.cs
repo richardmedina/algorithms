046d1bc52c06475faf77dc96b7a0afdd8d482121
Richard Medina <richardmedinalo@outlook.com>
2019-03-15 20:42:10 -0600

    Added Prototype Pattern implementation

---
+++ Algorithms/Algorithms.DesignPatterns/GangOfFour/Creational/Prototype/PersonalInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DesignPatterns.GangOfFour.Creational.Prototype
+{
+    public class PersonalInformation : ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        
+        public object Clone()
+        {
+            return new PersonalInformation
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                DateOfBirth = DateOfBirth
+            };
+        }
+    }
+}
