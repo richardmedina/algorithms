9acd9332c4962d986406d708e4ba96e08d38a8c7
Richard Medina <richardmedinalo@outlook.com>
2019-03-21 19:47:46 -0600

    Added Adapter Design Pattern Implementation

---
+++ Algorithms/Algorithms.DesignPatterns/GangOfFour/Structural/Adapter/MacOperativeSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DesignPatterns.GangOfFour.Structural.Adapter
+{
+    public class MacOperativeSystem : IOperativeSystem
+    {
+        public void ShutDown()
+        {
+            Console.WriteLine("Mac is shutting down ...Done");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Mac is turning o ...Done");
+        }
+    }
+}
---
+++ Algorithms/Algorithms.DesignPatterns/GangOfFour/Structural/Adapter/WindowsOperativeSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DesignPatterns.GangOfFour.Structural.Adapter
+{
+    public class WindowsOperativeSystem : IOperativeSystem
+    {
+        public void ShutDown()
+        {
+            Console.WriteLine("Windows ShutDown");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Windows TurnOn");
+        }
+    }
+}
