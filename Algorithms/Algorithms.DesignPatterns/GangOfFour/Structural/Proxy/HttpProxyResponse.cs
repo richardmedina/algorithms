4b801510d8c1634af05789166df8e0d3db9aa6ac
Richard Medina <richardmedinalo@outlook.com>
2019-04-21 05:37:07 -0500

    Proxy Design Pattern implementation

---
+++ Algorithms/Algorithms.DesignPatterns/GangOfFour/Structural/Proxy/HttpProxyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DesignPatterns.GangOfFour.Structural.Proxy
+{
+    public class HttpProxyResponse
+    {
+        public Dictionary<string, string> Headers;
+        public object Body { get; set; }
+        public int HttpStatusCode { get; set; }
+    }
+}
