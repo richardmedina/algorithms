523b82121ffbdd163c44e8b1adbba9cde24b5093
Richard Medina <richardmedinalo@outlook.com>
2019-07-06 05:15:56 -0500

    String Shuffle Algorithm Implementation

---
+++ Algorithms/Algorithms.CustomAlgorithms/StringShuffle/StringShuffleAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.CustomAlgorithms.StringShuffle
+{
+    public class StringShuffleAlgorithm : ICustomAlgorithm<string, string>
+    {
+        public string Run(string input)
+        {
+            var result = new StringBuilder(input);
+            var random = new Random();
+            var i = 0;
+            while (input.Length > 0)
+            {
+                var idx = random.Next(input.Length - 1);
+                result[i ++] = input[idx];
+                input = input.Remove(idx, 1);
+            }
+
+            return result.ToString();
+        }
+    }
+}
