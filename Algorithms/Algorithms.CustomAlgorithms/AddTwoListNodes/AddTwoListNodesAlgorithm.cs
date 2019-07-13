c25ed3028a210e0ee31d05fcaee070a4e29951cc
Richard Medina <richardmedinalo@outlook.com>
2019-07-13 01:02:17 -0500

    Blocks Algorithm Implementation AddToLisTNodes Implementation TwoNumbers Implementation

---
+++ Algorithms/Algorithms.CustomAlgorithms/AddTwoListNodes/AddTwoListNodesAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.CustomAlgorithms.AddTwoListNodes
+{
+    /// <summary>
+    /// Adds two custom linked lists, each list represents a number splited by digits as items in the list node
+    /// Input: 
+    /// list1 = [2, 4, 3] = 342
+    /// list2 = [5, 6, 4] = 465
+    /// result =  [7, 0, 8] = 807
+    /// </summary>
+    public class AddTwoListNodesAlgorithm : ICustomAlgorithm<ListNode, AddTwoListNodesArguments>
+    {
+        public ListNode Run(AddTwoListNodesArguments param)
+        {
+            ListNode result = new ListNode(0);
+
+            var iter1 = param.ListNode1;
+            var iter2 = param.ListNode2;
+            var iterresult = result;
+            var carry = 0;
+            var sum = 0;
+            do
+            {
+                var val1 = iter1 != null ? iter1.val : 0;
+                var val2 = iter2 != null ? iter2.val : 0;
+
+                sum = val1 + val2 + carry;
+                carry = 0;
+                iterresult.next = new ListNode(sum);
+
+                if (sum >= 10)
+                {
+                    carry = 1;
+                    iterresult.next.val = sum - 10;
+                }
+
+                iter1 = iter1?.next;
+                iter2 = iter2?.next;
+
+                iterresult = iterresult.next;
+            } while (iter1 != null || iter2 != null);
+
+            if (carry > 0)
+                iterresult.next = new ListNode(carry);
+
+
+            return result.next;
+        }
+    }
+}
---
+++ Algorithms/Algorithms.CustomAlgorithms/Blocks/BlocksAlgorithm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.CustomAlgorithms.Blocks
+{
+    public class BlocksAlgorithm : ICustomAlgorithm<int, string[]>
+    {
+        public int Run(string[] blocks)
+        {
+            var scores = new List<Score>();
+
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                var block = blocks[i];
+                var prev = scores.Any() ? scores[scores.Count - 1] : null;
+                Score score = null;
+
+                if (block == "Z")
+                {
+                    if (scores.Any())
+                    {
+                        scores.RemoveAt(scores.Count - 1);
+                    }
+                    continue;
+                }
+
+                if (block == "X")
+                {
+                    if (prev != null)
+                    {
+                        score = new Score();
+                        score.Current = 2 * prev.Current;
+                        score.Total = prev.Total + score.Current;
+
+                        scores.Add(score);
+                    }
+
+                    continue;
+                }
+
+                if (block == "+")
+                {
+                    var prevPrev = scores.Count > 2 ? scores[scores.Count - 2] : null;
+                    if (prev != null && prevPrev != null)
+                    {
+                        score = new Score();
+                        score.Current = prev.Current + prevPrev.Current;
+                        score.Total = prev.Total + score.Current;
+                        scores.Add(score);
+                    }
+                    continue;
+                }
+
+                var current = int.Parse(block);
+                score = new Score();
+                if (prev != null)
+                {
+                    score.Current = current;
+                    score.Total = prev.Total + score.Current;
+                }
+                else
+                {
+                    score.Current = current;
+                    score.Total = current;
+                }
+                scores.Add(score);
+            }
+
+
+            return scores.Any ()
+                ? scores.Last ().Total
+                : 0;
+        }
+    }
+
+    class Score
+    {
+        public int Current { get; set; }
+        public int Total { get; set; }
+    }
+}
