cb0faaab64f2504775ba962e6b46352a828f886d
Richard Medina <richardmedinalo@outlook.com>
2019-07-30 01:56:07 -0500

    Topologic Sort for Graphs

---
+++ Algorithms/Algorithms.DataStructures/GraphsTheory/Graph.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DataStructures.GraphsTheory
+{
+    public class Graph
+    {
+        public int[,] Adjacency { get; set; }
+        int[] InDegree { get; set; }
+        public int NodeCount { get; set; }
+
+        public Graph(int nodeCount)
+        {
+            NodeCount = nodeCount;
+            Adjacency = new int[NodeCount, NodeCount];
+            InDegree = new int[NodeCount];
+        }
+
+        public void AddArista (int startNode, int endNode, int weight)
+        {
+            Adjacency[startNode, endNode] = weight;
+        }
+
+        public void ShowAdjacency ()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                Console.Write($"\t{i}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{i}\t");
+
+                for (int j = 0; j < NodeCount; j ++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{Adjacency[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        /// <summary>
+        /// Calculates Indegree for each node in the Adjacency Matrix
+        /// InDegree is the number of Aristas that come to it
+        /// </summary>
+        public void CalculateIndegree ()
+        {
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                for (int j = 0; j < NodeCount; j ++)
+                {
+                    if (Adjacency [j, i] > 0)
+                    {
+                        InDegree[i]++;
+                    }
+                }
+            }
+        }
+        
+        public void ShowInDegree ()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                Console.WriteLine($"Node {i} = {InDegree[i]}");
+            }
+        }
+
+        public int FindInDegreeZero ()
+        {
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                if (InDegree[i] == 0) return i;
+            }
+
+            return -1;
+        }
+
+        public void DecrementInDegree (int node)
+        {
+            InDegree[node] = -1;
+
+            for (int i = 0; i < NodeCount; i ++)
+            {
+                if (Adjacency[node, i] == 1)
+                {
+                    InDegree[i]--;
+                }
+            }
+        }
+
+        public void TopologicSort ()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            var node = 0;
+            do
+            {
+                node = FindInDegreeZero();
+
+                if (node != -1)
+                {
+                    Console.Write($"{node}-> ");
+                    DecrementInDegree(node);
+                }
+
+            } while (node != -1);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Tests/DataStructures/GraphTests.cs
@@ -0,0 +1,41 @@
+﻿using Algorithms.DataStructures.GraphsTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.DataStructures
+{
+    [TestClass]
+    public class GraphTests
+    {
+
+        [TestMethod]
+        public void GraphsTopologicTest ()
+        {
+            var graph = new Graph(7);
+
+            graph.AddArista(0, 1, 1);
+            graph.AddArista(0, 2, 1);
+            graph.AddArista(0, 3, 1);
+
+            graph.AddArista(1, 3, 1);
+            graph.AddArista(1, 4, 1);
+            graph.AddArista(2, 5, 1);
+
+            graph.AddArista(3, 2, 1);
+            graph.AddArista(3, 5, 1);
+            graph.AddArista(3, 6, 1);
+
+            graph.AddArista(4, 3, 1);
+            graph.AddArista(4, 6, 1);
+            graph.AddArista(6, 5, 1);
+
+            graph.ShowAdjacency();
+            graph.CalculateIndegree();
+            graph.ShowInDegree();
+
+            graph.TopologicSort();
+        }
+    }
+}
